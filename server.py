00af15235a7bcffb60e7a1b1ddd734a85e27c663
benjadahl <benjadahl@gmail.com>
2016-06-15 23:52:55 +0200

    Added groups page, and created db.py file

--- server.py
+++ server.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
+
+#Import packages
 from flask import *
+import sqlite3
+
+#Start flask app
 app = Flask(__name__)
 
 #Default index page
 @app.route('/')
 def hello_word():
-    return "Hello, World!"
+    return "Welcome to Benja World!"
 
 #The town page, the game will automatically direct the page to your town when accesing this URL
 @app.route('/town/')
 def showTown():
     return render_template("town.html")
 
-@app.route('/t/<username>')
+@app.route('/group/')
+def showGroup():
+    return render_template("group.html")
+
+@app.route('/g/<username>')
 def show_user(username):
-    return "Hello, World! " + username
+    return "This is the group of: " + username
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
