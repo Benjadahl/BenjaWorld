42babf9cde372e1e2edca233c063753f3b6cbe57
benjadahl <benjadahl@gmail.com>
2016-06-17 01:07:59 +0200

    Begun integration of Facebook login

--- dbHandler.py
+++ dbHandler.py
@@ -3,15 +3,24 @@ import sqlite3
 
 def init(dbPath):
     print("[DB] Starting database")
+    global c
+    global db
     db = sqlite3.connect(dbPath + "/groups.db")
     c = db.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS groupses
-             (date text, trans text, symbol text, qty real, price real)''')
-    db.commit()
-    db.close()
 
-def test():
-    print("dank test man")
+def newGroup(username):
+    print("[DB] Creating new group")
+    c.execute("CREATE TABLE IF NOT EXISTS " + username + " (posX INTEGER, posY INTEGER)")
+
+def end():
+    try:
+        db.commit()
+        db.close()
+        print("[DB] Closing the connection to the database was succesful")
+        return True
+    except:
+        print("[DB] Closing the connection to the database failed")
+        return False
 
 if __name__ == "__main__":
-    print("This is a library you fool!")
+    print("This is a module you fool!")
--- server.py
+++ server.py
@@ -11,8 +11,13 @@ app = Flask(__name__)
 
 #Default index page
 @app.route('/')
-def hello_word():
+def showStart():
     return "Welcome to Benja World!"
+
+#The town page, the game will automatically direct the page to your town when accesing this URL
+@app.route('/login/')
+def showLogin():
+    return render_template("login.html")
 
 #The town page, the game will automatically direct the page to your town when accesing this URL
 @app.route('/town/')
@@ -24,10 +29,12 @@ def showGroup():
     return render_template("group.html")
 
 @app.route('/g/<username>')
-def show_user(username):
+def showUser(username):
     return "This is the group of: " + username
 
 if __name__ == "__main__":
     #Initailize the database
     dbHandler.init("./database/")
+    dbHandler.newGroup("dankMemeMan")
+    dbHandler.end()
     app.run(debug=False, host="0.0.0.0")
